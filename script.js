d58ccc19532cf98165a5a66abb9ae487b01a30f0
Almaz <almaz_73@mail.ru>
2023-02-25 08:47:36 +0300

    remember opened row

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var acc = document.getElementsByClassName("accordion");
+let selectedId = localStorage.getItem('selectedId');
 var i;
 
+// добавляем идентификатор
+for (i = 0; i < acc.length; i++) {
+    acc[i].id = i;
+    if (selectedId == i) {
+        var panel = acc[i].nextElementSibling;
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        acc[i].classList.toggle("active");
+    }
+}
+
+// добавляем слушателей к строкам
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
+        openRow(this)
     });
+}
+
+// При каждом нажатии скрыть открытые поля
+function closeAllRow() {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].nextElementSibling.style.maxHeight = null;
+    }
+}
+
+// Отерываение при нажатии но строку
+function openRow(me) {
+    console.log(me)
+    localStorage.setItem('selectedId', me.id); // запоминаем открутое поле
+    closeAllRow()
+    me.classList.toggle("active");
+    var panel = me.nextElementSibling;
+    panel.style.maxHeight = panel.scrollHeight + "px";
 }
