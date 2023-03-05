bf17f3e7f17b6668e0bec2cafae9d54574230557
almaz <almaz_73@mail.ru>
2023-03-05 08:19:58 +0300

    buttons done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 let selectedId = localStorage.getItem('selectedId') || '';
 let done = localStorage.getItem('done') || '';
 var i;
-let qqq = 0;
+let counter = 0;
 
 // добавляем идентификатор
 for (i = 0; i < acc.length; i++) {
@@ -21,11 +21,13 @@ for (i = 0; i < acc.length; i++) {
 
 // Отерываение при нажатии но строку
 function openRow(me) {
+    if (me.target.offsetWidth - me.offsetX < 40) return addDone(me, 'parent')
+
     // При каждом нажатии скрыть открытые поля
     Object.keys(acc).map(el => acc[el].nextElementSibling.style.maxHeight = null)
 
-    qqq++
-    if (qqq % 2 && selectedId === me.target.id) return
+    counter++
+    if (counter % 2 && selectedId === me.target.id) return
 
     selectedId = me.target.id
     localStorage.setItem('selectedId', selectedId); // запоминаем открытое поле
@@ -41,20 +43,23 @@ document.querySelectorAll('[type="checkbox"]').forEach(el => {
         el.checked = true
         el.parentElement.previousElementSibling.classList.toggle("done")
     }
-    el.addEventListener("click", handler)
+    el.addEventListener("click", addDone)
 })
 
-
-function handler(el) {
+function addDone(el, parent) {
+    let element = el.target.parentElement.previousElementSibling
+    if (parent) element = el.target
     let doneArr = done.split(',').map(el => +el)
-    let numberTask = +el.target.parentElement.previousElementSibling.id + 1
+    let numberTask = +element.id + 1
     if (doneArr.indexOf(numberTask) > 0) {
         let index = doneArr.indexOf(numberTask)
-        el.target.parentElement.previousElementSibling.classList.remove("done")
+        element.classList.remove("done")
         doneArr.splice(index, 1)
+        if (parent) el.target.nextElementSibling.querySelector('input').checked = false
     } else {
         doneArr.push(numberTask)
-        el.target.parentElement.previousElementSibling.classList.add("done")
+        element.classList.add("done")
+        if (parent) el.target.nextElementSibling.querySelector('input').checked = true
     }
     done = doneArr.join(',')
     localStorage.setItem('done', done)
